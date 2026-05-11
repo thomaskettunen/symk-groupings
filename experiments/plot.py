1aff0ba4a19ee58732c9213bd9d0ddc7a5cf3fd1
MBStarup <mavi.nexu@gmail.com>
2026-05-11 15:11:46 +0200

    Added scatterplot

--- experiments/plot.py
+++ experiments/plot.py
@@ -75,14 +75,14 @@ def main():
   plt.close()
 
   ## progressive k
-  ks = list(range(1, 6))
+  ks = list(range(2, 6))
   for grouping in groupings:
     for search in searches:
       coverages = [0 for k in ks]
       for k in ks:
         for data in [run for run in runs if run.has({"search": search, "grouping": grouping})]:
           if data["plans_found"] >= k or data["exit_code"] == "ExitCode.FOUND_ALL":
-            coverages[k-1] += 1
+            coverages[k-2] += 1
 
       plt.plot(ks, coverages, label = search)
 
@@ -137,6 +137,35 @@ def main():
     plt.legend()
 
     plt.savefig(f"{ensure_dir(f'{plot_dir}/exhausted_search_vs_last_plan')}/{grouping}.png", dpi=300, bbox_inches="tight")
+    plt.close()
+
+  ## total time scatter
+  #! Assumes just two searches
+  selected = (searches[0], searches[1])
+  step = 1000 #. grid steps
+  for grouping in groupings:
+    problems = [(run["domain"], run["problem"]) for run in runs if run.has({"grouping": grouping})] # Extract problems first to ensure same order
+    times = {search: [] for search in searches}
+    max_time = max([run["total_time"] for run in runs if run.has({"grouping": grouping})])
+    limit = ((int(max_time * 1.10) + step - 1) // step) * step #. round to step, so it is exactly at the line in the plot
+    for (domain, problem) in problems:
+      for search in searches:
+        key = {"domain": domain, "problem": problem, "grouping": grouping, "search": search}
+        time = [run["total_time"] if run["coverage"] else limit for run in runs if run.has(key)]
+        if not len(time) == 1: raise RuntimeError(f"Expected exactly one run with {key}, found: {len(time)}")
+        times[search] += time
+    plt.scatter(times[selected[0]], times[selected[1]])
+
+    plt.xlabel(f"Time {selected[0]}")
+    plt.ylabel(f"Time {selected[1]}")
+    ticks = list(range(0, limit + step, step))
+    labels = list(map(str, ticks))[:-1] + ["∞"]
+    plt.xticks(ticks, labels)
+    plt.yticks(ticks, labels)
+    plt.title(f"Total Time {selected[0]} vs {selected[1]} ({grouping})")
+    plt.grid(True)
+
+    plt.savefig(f"{ensure_dir(f'{plot_dir}/total_time_vs')}/{grouping}.png", dpi=300, bbox_inches="tight")
     plt.close()
 
 if __name__ := "__main__":
